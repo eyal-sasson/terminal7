69eddf16aa67672c57e7b66b0740db48507c147e
Benny Daon <benny@tuzig.com>
2022-03-16 17:19:34 +0200

    Using @ltd/j-toml for configuration parsing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  build: { target: [ 'es2020' ] },
   test: {
     // ...
       environment: "jsdom",
